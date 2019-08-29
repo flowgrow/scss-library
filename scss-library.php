7feeb96d1dcb8ce603fa8736a501a629ead51ed6
Juan Sebastián Echeverry <baxtian.echeverry@gmail.com>
2019-08-29 12:09:55 -0500

    Documentación.

--- scss-library.php
+++ scss-library.php
@@ -31,13 +31,13 @@ use ScssPhp\ScssPhp\Compiler;
 class SassLibrary
 {
 	/**
-	 * An array of any compilation errors.
+	 * Arreglo para guardar los errores de compliación.
 	 * @var array
 	 */
 	protected $errors = array();
 
 	/**
-	 * Create an instance.
+	 * Crear la instancia.
 	 * @return void
 	 */
 	public function __construct()
@@ -54,37 +54,37 @@ class SassLibrary
 		load_plugin_textdomain('scsslib', false, basename(__DIR__) . '/languages');
 	}
 
-	/**
-	 * Hook into wp_enqueue_style to compile stylesheets
-	 */
+	// Función para compilar los estilos SCSS.
 	public function style_loader_src($src, $handle)
 	{
 
-		// Quick check for SCSS files
+		// Si el nombre el archivo no tiene el texto scss entonces
+		// retornar el estilo sin cambios
 		if (strpos($src, 'scss') === false) {
 			return $src;
 		}
 
+		// Parsear la URL del archivo de estilo
 		$url = parse_url($src);
 		$pathinfo = pathinfo($url['path']);
 
-		// Detailed check for SCSS files
+		// Revisión detallada para determinar si la extensión corresponde
 		if ($pathinfo['extension'] !== 'scss') {
 			return $src;
 		}
 
-		// Convert site URLs to absolute paths
+		// Convertir la URL a rutas absolutas.
 		$in = preg_replace('/^' . preg_quote(site_url(), '/') . '/i', '', $src);
 
-		// Ignore SCSS from CDNs, other domains and relative paths
+		// Ignorar SCSS de CDNs, otros dominios y rutas relativos
 		if (preg_match('#^//#', $in) || strpos($in, '/') !== 0) {
 			return $src;
 		}
 
-		// Create a complete path
+		// Crear ruta completa
 		$in = rtrim($_SERVER['DOCUMENT_ROOT'], '/') . $url['path'];
 
-		// Check and make sure the file exists
+		// Confirmar que el archivo fuente existe
 		if (file_exists($in) === false) {
 			array_push($this->errors, array(
 				'file'    => basename($in),
@@ -93,10 +93,10 @@ class SassLibrary
 			return $src;
 		}
 
-		// Generate unique filename for output
+		// Generar nombre único paa el archivo compilado
 		$outName = sha1($src) . '.css';
 
-		//Directorio donde se almacenará el cache
+		// Directorio donde se almacenará el cache
 		$pathname = '/cache/scss_library/';
 		if ( is_multisite() ) {
             $blog_id   = get_current_blog_id();
@@ -107,7 +107,7 @@ class SassLibrary
 		$outputDir = WP_CONTENT_DIR . $pathname;
 		$outputUrl = WP_CONTENT_URL .  $pathname . $outName;
 
-		// Create the output directory if it doesn't exisit
+		// Crear el directorio de archivos compilados si no existe
 		if (is_dir($outputDir) === false) {
 			if (wp_mkdir_p($outputDir) === false) {
 				array_push($this->errors, array(
@@ -119,7 +119,8 @@ class SassLibrary
 			}
 		}
 
-		// Check that the output directory is writable
+		// Revisar que el directorio donde se almacenarán los archivos
+		// compilados tiene permisos de escritura
 		if (is_writable($outputDir) === false) {
 			array_push($this->errors, array(
 				'file'    => 'Cache Directory',
@@ -129,32 +130,33 @@ class SassLibrary
 			return $src;
 		}
 
-		// Full output path
+		// Ruta comleta para el archivo compilado
 		$out = $outputDir . $outName;
 
-		// Flag if a compile is required
-		// $compileRequired = $this->admin->get_setting('always_compile', false);
+		// Bandera para saber si se requiere compilar el archivo. Por defecto suponemos
+		// que no es necesario compilar.
 		$compileRequired = false;
 
-		// Retrieve cached filemtimes
+		// Obtener la fecha que tenemos almacenada como fecha de creación de cada archivos
 		if (($filemtimes = get_transient('scsslib_filemtimes')) === false) {
 			$filemtimes = array();
 		}
 
-		// Check if compile is required based on file modification times
+		// Compara la fecha de creación del archivo compilado con la fecha de creación del
+		// archivo fuente. Si es más reciente entones hay que compilar.
 		if ($compileRequired === false) {
 			if (isset($filemtimes[$out]) === false || $filemtimes[$out] < filemtime($in)) {
 				$compileRequired = true;
 			}
 		}
 
-		// Retrieve variables
+		// Obtener las variables variables
 		$variables = apply_filters('scsslib_compiler_variables', array(
 			'template_directory_uri'   => get_template_directory_uri(),
 			'stylesheet_directory_uri' => get_stylesheet_directory_uri(),
 		));
 
-		// If variables have been updated then recompile
+		// Si las variables no coinciden entonces hay que compilar
 		if ($compileRequired === false) {
 			$signature = sha1(serialize($variables));
 			if ($signature !== get_transient('scsslib_variables_signature')) {
@@ -163,35 +165,39 @@ class SassLibrary
 			}
 		}
 
-		// $formatter = $this->admin->get_setting('compiling_mode', 'Leafo_ScssPhp_Formatter_Expanded');
+		// Tipo de formato por defecto
 		$formatter = 'ScssPhp\ScssPhp\Formatter\Expanded';
 
-		// Check if the stylesheet needs to be recompiled
+		// ¿Debemos o no compilar?
 		if ($compileRequired) {
+			// Inicializar compilador
 			$compiler = new Compiler();
 
+			// Determinar las varianles para el archivo de depuración
 			$srcmap_data = array(
-				// absolute path to write .map file
+				// Ruta absoluta donde se escribirá el archivo .map
 				'sourceMapWriteTo'  => $outputDir . $outName . ".map",
-				// relative or full url to the above .map file
+				// URL completa o relativa al archivp .map
 				'sourceMapURL'      => $outputUrl . ".map",
-				// (optional) relative or full url to the .css file
+				// (Opcional) URL relativa o completa al archivo .css compilado
 				'sourceMapFilename' => $outputUrl,
-				// partial path (server root) removed (normalized) to create a relative url
+				// Ruta parcial (raiz del servidor) para crear la URL relativa
 				'sourceMapBasepath' => rtrim(ABSPATH, '/'),
-				// (optional) prepended to 'source' field entries for relocating source files
+				// (Opcional) Antepuesto a las entradas de campo 'fuente' para reubicar archivos fuente
 				'sourceRoot'        => $src,
 			);
 
-			$compiler->setSourceMap(Compiler::SOURCE_MAP_FILE);  // SOURCE_MAP_NONE, SOURCE_MAP_INLINE, or SOURCE_MAP_FILE
+			// Configuración para crear el archivo .map de depuración.
+			$compiler->setSourceMap(Compiler::SOURCE_MAP_FILE);
 			$compiler->setSourceMapOptions($srcmap_data);
 
+			// Configuración para inicializar el compilador.
 			$compiler->setFormatter($formatter);
 			$compiler->setVariables($variables);
 			$compiler->setImportPaths(dirname($in));
 
 			try {
-				// Compile the SCSS to CSS
+				// Compilar de SCSS a CSS
 				$css = $compiler->compile(file_get_contents($in), $in);
 			} catch (Exception $e) {
 				array_push($this->errors, array(
@@ -201,44 +207,32 @@ class SassLibrary
 				return $src;
 			}
 
-			// Transform relative paths so they still work correctly
+			// Transformar las rutas relativas para que funcionen correctamente
 			$css = preg_replace('#(url\((?![\'"]?(?:https?:|/))[\'"]?)#miu', '$1' . dirname($url['path']) . '/', $css);
 
-			// Save the CSS
+			// Guardar el archivo compilado.
 			file_put_contents($out, $css);
 
-			// Cache the filemtime for the destination file
+			// Guardar el tiempo de creación del archivo.
 			$filemtimes[$out] = filemtime($out);
 			set_transient('scsslib_filemtimes', $filemtimes);
 		}
 
-		// Build URL with query string
+		// Construir URL del archivio compilado con las cadenas de consulta que
+		// venían en la URL del archivo fuente.
 		return empty($url['query']) ? $outputUrl : $outputUrl . '?' . $url['query'];
 	}
 
-	/**
-	 * Output any errors in the footer.
-	 */
+	// Publicar errores
 	public function wp_footer()
 	{
-		// $error_mode = $this->admin->get_setting('errors_mode');
-		$error_mode = 'error_display';
-
+		//Si hay errores, visualizarlos
 		if (count($this->errors)) {
-			switch ($error_mode) {
-				case 'error_log':
-					$this->logErrors();
-					break;
-
-				default:
-					$this->displayErrors();
-			}
+			$this->displayErrors();
 		}
 	}
 
-	/**
-	 * Display HTML formatted errors.
-	 */
+	// HTML para visualizar errores.
 	protected function displayErrors()
 	{
 		?>
@@ -289,16 +283,6 @@ class SassLibrary
 			<?php endforeach ?>
 		</div>
 		<?php
-	}
-
-	/**
-	 * Log errors.
-	 */
-	protected function logErrors()
-	{
-		foreach ($this->errors as $error) {
-			error_log($error['file'] . ': ' . $error['message']);
-		}
 	}
 }
 
