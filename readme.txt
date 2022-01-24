fdd4d00b3b5bdff06a2409aeb42b9cd278b720ac
Juan Sebastian Echeverry <baxtian.echeverry@gmail.com>
2022-01-24 09:41:00 -0500

    Cambios sugeridos para wp-cli

--- readme.txt
+++ readme.txt
@@ -3,9 +3,9 @@
 Contributors: sebaxtian  
 Tags: SASS, compiler, SCSS  
 Requires at least: 4.4  
-Tested up to: 5.2.2  
+Tested up to: 5.5  
 Stable tag: trunk  
-Requires PHP: 5.0  
+Requires PHP: 7.1  
 License: GPLv2 or later  
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
--- scss-library.php
+++ scss-library.php
@@ -3,10 +3,12 @@
 Plugin Name: SCSS-Library
 Description: Adds support for SCSS stylesheets to wp_enqueue_style.
 Author: Juan Sebastián Echeverry
-Version: 0.2.7
+Version: 0.2.11
+Tested up to: 5.8.3
+Requires PHP: 7.1
 Text Domain: scsslib
 
-Copyright 2019-2020 Juan Sebastián Echeverry (baxtian.echeverry@gmail.com)
+Copyright 2019-2022 Juan Sebastián Echeverry (baxtian.echeverry@gmail.com)
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -24,8 +26,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 // Si estamos usando wp-cli, no correr el plugin
-$autoloader = __DIR__ . '/vendor/autoload.php';
-if (!is_readable($autoloader)) {
+if (defined('WP_CLI') && WP_CLI) {
 	return;
 }
 
