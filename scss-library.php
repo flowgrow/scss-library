ac8ac0469642df25c37d2a804adcfaf1ca17f4df
Florian Groh <florian.groh@tuwien.ac.at>
2022-04-02 14:10:13 +0200

    change author

--- scss-library.php
+++ scss-library.php
@@ -2,14 +2,15 @@
 /*
 Plugin Name: SCSS-Library
 Description: Adds support for SCSS stylesheets to wp_enqueue_style.
-Author: Juan Sebastián Echeverry
+Author: Florian Groh
 GitHub Plugin URI: flowgrow/scss-library
-Version: 0.2.11
-Tested up to: 5.8.3
+Version: 0.2.12
+Tested up to: 5.9.2
 Requires PHP: 7.1
 Text Domain: scsslib
 
 Copyright 2019-2022 Juan Sebastián Echeverry (baxtian.echeverry@gmail.com)
+Copyright 2022-2022 Florian Groh (florian@kniff.at)
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
