52f66e6ebc6be1fec0a0d74b38101cf0a10c316b
Florian Groh <florian.groh@tuwien.ac.at>
2022-06-23 14:32:15 +0200

    version bump

--- scss-library.php
+++ scss-library.php
@@ -6,7 +6,7 @@ Author: Florian Groh
 GitHub Plugin URI: flowgrow/scss-library
 Primary Branch: master
 Release Asset: true
-Version: 0.3.7
+Version: 0.3.8
 Tested up to: 5.9.2
 Requires PHP: 7.1
 Text Domain: scsslib
